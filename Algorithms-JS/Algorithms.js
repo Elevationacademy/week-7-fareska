59e20b639829d78ca367173110dea5ca737a8ed3
fareska <fares.xb@gmail.com>
2021-02-01 19:04:04 +0200

    finished extintion 2

--- Algorithms-JS/Algorithms.js
+++ Algorithms-JS/Algorithms.js
@@ -44,26 +44,86 @@ const printStarSeries = (num, count) => {
 
 
 //////////////Exercise 4
-const reverse = function(str){
-    
-    let reversed =""
-    for (let i=0; i<str.length; i++){
-        reversed += str.charAt(str.length-1-i)
+const reverse = function (str) {
+
+    let reversed = ""
+    for (let i = 0; i < str.length; i++) {
+        reversed += str.charAt(str.length - 1 - i)
     }
     return reversed
 }
-
 //console.log(reverse("dog")) //should return "god"
 //console.log(reverse("race car")) //should return "rac ecar"
 
 
 //////////////Exercise 5
-const isPalindrom = function(str){
-    let reversed = reverse(str).toLowerCase().replace(/ /g,'') 
-    if (reversed === str.toLowerCase().replace(/ /g,'')){
-        return true}
-    else{return false } 
-    
+const isPalindrom = function (str) {
+    let reversed = reverse(str).toLowerCase().replace(/ /g, '')
+    if (reversed === str.toLowerCase().replace(/ /g, '')) {
+        return true
+    }
+    else { return false }
+}
+//console.log(isPalindrom('Taco Cat')) // should return true
+
+
+
+
+
+
+/////////////////////////////////////////////////
+//////////////Extension 1
+const printInLoop = words => {
+    words.forEach(w => console.log(w))
+    return printInLoop(words)
 }
 
-//console.log(isPalindrom('Taco Cat'))
+const arr = ["down", "the", "rabbit", "hole"]
+//printInLoop(arr) should print the array continually as long as the terminal running.
+
+/////////////////////////////////////////////////
+//////////////Extension 2
+const findMin = array => {
+    let n = array[0]
+    for (let a of array){
+        if(a<n){
+         n = a
+        }
+    }
+    return n
+} 
+// console.log(findMin([3, 4, 2, 1178, 244, 59, 10, 244, 718, 144]))
+
+let calculateDistance = (pointA, pointB) => {
+    let xSquared = ((pointA.x - pointB.x) * (pointA.x - pointB.x))
+    let ySquared = ((pointA.y - pointB.y) * (pointA.y - pointB.y))
+    let dSquared  = (xSquared + ySquared)// Math.sqrt(((yDistance * yDistance) + (xDistance * xDistance)))
+    let distance = Math.sqrt(dSquared)
+    return distance
+}
+
+const findClosest = (points, point) => {
+    let newArr = [] //Includes new key = distance
+    let distances = []
+    for (let p of points) {
+        let d = calculateDistance(point, p)
+        p.d = d
+        newArr.push(p)
+        distances.push(d)
+    }
+    let max = findMin(distances)
+    return newArr.find(p => p.d === max)
+}
+
+let pizzaLocations = [
+    { x: 6, y: 12 },
+    { x: 3, y: 1 },
+    { x: 9, y: 0 },
+    { x: 4, y: 10 }
+]
+
+let homeLocation = { x: 4, y: 2 }
+
+
+console.log(findClosest(pizzaLocations, homeLocation)) // should return {x: 3, y: 1}
+
